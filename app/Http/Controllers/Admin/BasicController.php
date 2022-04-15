ddd366a4a531be8729b71acbf53301a77737ca5c
quannguyen <hoangquan.it.hcm@gmail.com>
2022-04-16 01:32:02 +0700

    update base

---
+++ app/Http/Controllers/Admin/BasicController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BasicController extends Controller
+{
+    public function list() {
+        return 123;
+    }
+
+    public function getAdd() {
+        
+    }
+
+    public function postAdd() {
+        
+    }
+
+    public function getEdit() {
+        
+    }
+
+    public function postEdit() {
+        
+    }
+
+    public function delete() {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// use App\Http\Controllers\Admin\BasicController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +21,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::prefix('basic')->group(function() {
+        Route::get('list', [App\Http\Controllers\Admin\BasicController::class, 'list'])->name('basic-list');
+
+        Route::get('add', [App\Http\Controllers\Admin\BasicController::class, 'getAdd'])->name('basic-add');
+        Route::post('add', [App\Http\Controllers\Admin\BasicController::class, 'postAdd'])->name('post-basic-add');
+
+        Route::get('edit/{postMeta}', [App\Http\Controllers\Admin\BasicController::class, 'getEdit'])->name('basic-edit');
+        Route::post('edit/{postMeta}', [App\Http\Controllers\Admin\BasicController::class, 'postEdit'])->name('post-basic-edit');
+
+        Route::get('delete/{postMeta}', [App\Http\Controllers\Admin\BasicController::class, 'delete'])->name('basic-delete');
+    });
+});
