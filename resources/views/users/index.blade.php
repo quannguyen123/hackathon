b3343719146745ae462e942f7a3ac9e8ab82b58b
nguyen-van-duc <nguyen-van-duc@pdc.proto-g.co.jp>
2022-04-16 15:15:24 +0700

    create filter user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,25 +3,26 @@
 
 @section('content')
 <div class="content-wrapper">
-<section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Users</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{route("home")}}">{{_('Home') }}</a></li>
-              <li class="breadcrumb-item active">{{_('List users')}}</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Users</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{route("home")}}">{{_('Home') }}</a></li>
+                <li class="breadcrumb-item active">{{_('List users')}}</li>
+                </ol>
+            </div>
+            </div>
+        </div><!-- /.container-fluid -->
     </section>
     <section class="content">
       <div class="container-fluid">
             <div class="row">
                 <div class="col-md">
+                    @include('users.search')
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">{{ __('List of users') }}</h3>
