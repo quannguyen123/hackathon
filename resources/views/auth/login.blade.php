3bd4fa75e27739e40be2fe2d9016e7991cae4c31
CuongNQ <tm-vn@protosolution.co.jp>
2022-04-16 11:09:46 +0700

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,79 @@
 @extends('master-not-sidebar')
 
 @section('content')
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>FaceDown</b></a>
+    <body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+          <div class="card-header text-center">
+              <a href="https://jobs.hybrid-technologies.vn/" target="_blank" class="h1"><b>FaceDown</b></a>
+          </div>
+          <div class="card-body">
+              <p class="login-box-msg">Sign in to start your session</p>
+
+              <form action="{{ route('login') }}" method="post">
+                  @csrf
+                  <div class="input-group mb-3">
+                      <input
+                            type="email" 
+                            class="form-control @error('email') is-invalid @enderror" 
+                            name="email" 
+                            value="{{ old('email') }}" 
+                            required 
+                            placeholder="Email"
+                            autocomplete="email" 
+                            autofocus>
+                      <div class="input-group-append">
+                          <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                          </div>
+                      </div>
+                      @error('email')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                  </div>
+                  <div class="input-group mb-3">
+                      <input 
+                            id="password" 
+                            type="password" 
+                            class="form-control @error('password') is-invalid @enderror" 
+                            name="password" 
+                            required 
+                            placeholder="Password"
+                            autocomplete="current-password">
+                      <div class="input-group-append">
+                          <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                          </div>
+                      </div>
+                      @error('password')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                  </div>
+                  <div class="row">
+                      <div class="col-8">
+                          <div class="icheck-primary">
+                              <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                              <label for="remember">
+                                  Remember Me
+                              </label>
+                          </div>
+                      </div>
+                      <div class="col-4">
+                          <button type="submit" class="btn btn-primary btn-block">Login</button>
+                      </div>
+
+                  </div>
+              </form>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
     </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="../../index3.html" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="mail" value="{{ old('email') }}" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" value="{{ old('email') }}" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      
-      <!-- /.social-auth-links -->
-
-      <p class="mb-0">
-        <a href="/register" class="text-center">Register a new membership</a>
-      </p>
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
 </body>
 <!-- <div class="container">
     <div class="row justify-content-center">
