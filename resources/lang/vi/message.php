ba2a16158f5e7b7afc72015ffc6ae1c6c9a746c8
nguyen-van-duc <nguyen-van-duc@pdc.proto-g.co.jp>
2022-04-17 15:51:25 +0700

    fix lang

---
+++ resources/lang/vi/message.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    'system' => 'Hybrid Technologies',
+    'login' => 'Đăng nhập',
+    'logout' => 'Thoát',
+    'projects' => 'Dự án',
+    'manger' => 'Phụ trách dự án',
+    'users' => 'Người dùng',
+    'user_name' => 'Họ tên',
+    'phone_number' => 'Số điện thoại',
+    'start_date' => 'Ngày bắt đầu',
+    'end_date' => 'Ngày kết thúc',
+    'description' => 'Mô tả',
+    'remember' => 'Lưu đăng nhập',
+    'save' => 'Lưu',
+    'cancel' => 'Quay lại',
+    'list' => 'Danh sách',
+    'create' => 'Thêm mới',
+    'issues' => 'Vấn đề',
+    'email' => 'Email',
+    'password' => 'Mật khẩu',
+    'issue' => [
+        'create' => 'Tạo vấn đề',
+        'edit' => 'Thay đổi vấn đề',
+        'title' => 'Tiêu đề',
+        'required' => 'Vui lòng nhập vấn đề',
+        'max' => 'Độ dài không vượt quá 255',
+        'min' => 'Độ dài không nhỏ hơn 5',
+        'security' => 'Quyền công khai ',
+        'public' => 'Công khai',
+        'private' => 'Riêng tư'
+    ],
+    'list' => 'Danh sách',
+    'role' => 'Vai trò',
+    'create_at' => 'Ngày tạo',
+    'user' => [
+        'create' => 'Tạo vấn đề',
+        'edit' => 'Thay đổi vấn đề',
+        'list' => 'Danh sách',
+    ],
+    'fields' => [
+        'issue' => [
+            'required' => 'Vui lòng nhập vấn đề',
+            'max' => 'Độ dài không vượt quá 255',
+            'min' => 'Độ dài không nhỏ hơn 5',
+        ],
+        'email' => [
+            'required' => 'Vui lòng nhập email',
+            'format' => 'Nhập email đúng định dạng',
+            'exists' => 'Email không tồn tại trong hệ thống',
+        ],
+        'phone_number' => [
+            'max' => 'Độ dài không vượt quá 255',
+            'required' => 'Vui lòng nhập số điện thoại',    
+        ],
+        
+    ],
+    'home' => 'Trang chủ'
+];
