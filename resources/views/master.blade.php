6422866412a4f777349f4b726ff36de06fb90ac7
quannguyen <nguyen-van-quan@pdc.proto-g.co.jp>
2022-04-17 17:06:58 +0700

    update code

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -99,7 +99,7 @@
     <!-- Brand Logo -->
     <a href="{{route("users.dashboard")}}" class="brand-link">
       <img src="{{ asset('adminlte/dist/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+      <span class="brand-text font-weight-light">Hybrid Technologies</span>
     </a>
     @include('partials.sidebar')
     <!-- /.sidebar -->
