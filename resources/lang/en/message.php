4b8bd14cfd8198e19de468a7b3922295f384feea
CuongNQ <tm-vn@protosolution.co.jp>
2022-04-17 14:04:51 +0700

    update translate and check roles

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -4,9 +4,10 @@ return [
     'system' => 'Hybrid Techonologies',
     'login' => 'Đăng nhập',
     'logout' => 'Thoát',
-    'project' => 'Dự án',
+    'projects' => 'Dự án',
     'manger' => 'Phụ trách dự án',
-    'user_name' => 'Họ và tên',
+    'users' => 'Người dùng',
+    'user_name' => 'Họ tên',
     'phone_number' => 'Số điện thoại',
     'start_date' => 'Ngày bắt đầu',
     'end_date' => 'Ngày kết thúc',
@@ -14,23 +15,39 @@ return [
     'remember' => 'Lưu đăng nhập',
     'save' => 'Lưu',
     'cancel' => 'Quay lại',
-    'email' => [
-        'title' => 'Email',
-        'required' => 'Vui lòng nhập email',
-        'format' => 'Nhập email đúng định dạng',
-        'exists' => 'Email không tồn tại trong hệ thống',
-    ],
-    'password' => [
-        'title' => 'Mật khẩu',
-        'required' => 'Vui lòng nhập mật khẩu',
-    ],
+    'list' => 'Danh sách',
+    'create' => 'Thêm mới',
+    'issues' => 'Vấn đề',
+    'email' => 'Email',
+    'password' => 'Mật khẩu',
     'issue' => [
         'create' => 'Tạo vấn đề',
         'edit' => 'Thay đổi vấn đề',
-        'title' => 'Tiêu đề',
-        'required' => 'Vui lòng nhập vấn đề',
-        'max' => 'Độ dài không vượt quá 255',
-        'min' => 'Độ dài không nhỏ hơn 5',
-        'private' => 'Riêng tư'
-    ]
+        'list' => 'Danh sách',
+    ],
+    'role' => 'Vai trò',
+    'create_at' => 'Ngày tạo',
+    'user' => [
+        'create' => 'Tạo vấn đề',
+        'edit' => 'Thay đổi vấn đề',
+        'list' => 'Danh sách',
+    ],
+    'fields' => [
+        'issue' => [
+            'required' => 'Vui lòng nhập vấn đề',
+            'max' => 'Độ dài không vượt quá 255',
+            'min' => 'Độ dài không nhỏ hơn 5',
+        ],
+        'email' => [
+            'required' => 'Vui lòng nhập email',
+            'format' => 'Nhập email đúng định dạng',
+            'exists' => 'Email không tồn tại trong hệ thống',
+        ],
+        'phone_number' => [
+            'max' => 'Độ dài không vượt quá 255',
+            'required' => 'Vui lòng nhập số điện thoại',    
+        ],
+        
+    ],
+    'home' => 'Trang chủ'
 ];
--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -13,145 +13,118 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="{{route("users.dashboard")}}">{{_('Home') }}</a></li>
-                <li class="breadcrumb-item active">{{_('List users')}}</li>
+                <li class="breadcrumb-item active">{{__('message.user.list')}}</li>
                 </ol>
             </div>
             </div>
         </div><!-- /.container-fluid -->
     </section>
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <form class="card" action="{{ empty($user) ? route('users.store', ['route_name' => request('route_name')]) : route('users.update', ['user' => $user->id, 'route_name' => request('route_name')]) }}" method="post">
-                    @csrf
-                    @if (!empty($user)) @method('PUT') @endif
+    <section class="content">    
+        <form action="{{ empty($user) ? route('users.store', ['route_name' => request('route_name')]) : route('users.update', ['user' => $user->id, 'route_name' => request('route_name')]) }}" method="post">
+            @csrf
+            @if (!empty($user)) @method('PUT') @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">{{ __('message.user_name') }}</label>
+                                <input
+                                    id="name"
+                                    type="text"
+                                    name="name"
+                                    class="form-control @error('name') is-invalid @enderror"
+                                    value="{{ old('name') ?: (!empty($user) ? $user->name : '') }}"
+                                    required
+                                />
 
-                    <div class="card-header">
-                        <h3 class="card-title">
-                            {{ !empty($user) ? __('Edit user: :name', ['name' => $user->name]) : __('Create User') }}
-                        </h3>
-
-                        <div class="card-tools">
-                            <a href="{{ route(request('route_name', 'users') . '.index') }}" class="btn btn-primary btn-sm">
-                                {{ __('List users') }}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">{{ __('Name') }}</label>
-                                    <input
-                                        id="name"
-                                        type="text"
-                                        name="name"
-                                        class="form-control @error('name') is-invalid @enderror"
-                                        value="{{ old('name') ?: (!empty($user) ? $user->name : '') }}"
-                                        required
-                                    />
-
-                                    @error('name')
-                                    <span class="error invalid-feedback" style="display: block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                                @error('name')
+                                <span class="error invalid-feedback" style="display: block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">{{ __('Phone') }}</label>
-                                    <input
-                                        id="phone_number"
-                                        type="text"
-                                        name="phone_number"
-                                        class="form-control @error('phone_number') is-invalid @enderror"
-                                        value="{{ old('phone_number') ?: (!empty($user) ? $user->phone_number : '') }}"
-                                    />
+                            <div class="form-group">
+                                <label for="name">{{ __('message.phone_number') }}</label>
+                                <input
+                                    id="phone_number"
+                                    type="text"
+                                    name="phone_number"
+                                    class="form-control @error('phone_number') is-invalid @enderror"
+                                    value="{{ old('phone_number') ?: (!empty($user) ? $user->phone_number : '') }}"
+                                />
 
-                                    @error('phone_number')
-                                    <span class="error invalid-feedback" style="display: block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                                @error('phone_number')
+                                <span class="error invalid-feedback" style="display: block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="email">{{ __('Email') }}</label>
-                                    <input
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                        class="form-control @error('email') is-invalid @enderror"
-                                        value="{{ old('email') ?: (!empty($user) ? $user->email : '') }}"
-                                        required
-                                    />
+                            <div class="form-group">
+                                <label for="email">{{ __('message.email') }}</label>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    value="{{ old('email') ?: (!empty($user) ? $user->email : '') }}"
+                                    required
+                                />
 
-                                    @error('email')
-                                    <span class="error invalid-feedback" style="display: block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                                @error('email')
+                                <span class="error invalid-feedback" style="display: block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="roles">{{ __('Roles') }}</label>
-                                    <select id="role_id" name="role_id" class="form-control select2bs4 @error('roles') is-invalid @enderror">
-                                        @foreach($roles as $role)
-                                            <option
-                                                value="{{ $role->id }}"
-                                                @if (!empty($user) && ($role->id == $user->role_id))
-                                                selected
-                                                @elseif ($role->id = old('role_id'))
-                                                selected
-                                                @endif
-                                            >
-                                                {{ $role->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                    @error('role_id')
-                                    <span class="error invalid-feedback" style="display: block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                            <div class="form-group">
+                                <label for="roles">{{ __('message.role') }}</label>
+                                <select id="role_id" name="role_id" class="form-control select2bs4 @error('roles') is-invalid @enderror">
+                                    @foreach($roles as $role)
+                                        <option
+                                            value="{{ $role->id }}"
+                                            @if (!empty($user) && ($role->id == $user->role_id))
+                                            selected
+                                            @elseif ($role->id = old('role_id'))
+                                            selected
+                                            @endif
+                                        >
+                                            {{ $role->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('role_id')
+                                <span class="error invalid-feedback" style="display: block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
+                            <div class="form-group">
+                                <label for="password">{{ __('message.password') }}</label>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    placeholder="{{ __('********') }}"
+                                />
 
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="password">{{ __('Password') }}</label>
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        name="password"
-                                        class="form-control @error('password') is-invalid @enderror"
-                                        placeholder="{{ __('********') }}"
-                                    />
-
-                                    @error('password')
-                                    <span class="error invalid-feedback" style="display: block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                                @error('password')
+                                <span class="error invalid-feedback" style="display: block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="float-left">
+                                <a href="{{ route(request('route_name', 'users') . '.index') }}" class="btn btn-secondary">{{ __('message.cancel') }}</a>
+                                <button type="submit" class="btn btn-primary">{{ __('message.create') }}</button>
                             </div>
                         </div>
                     </div>
-                    <div class="card-footer clearfix">
-                        <div class="float-right">
-                            <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-     </div>
+        </form>
+        
     </section>
 <!-- /.content -->
 </div>
